cebd7fdb8be151e6d08be43bf2c34163373709bd
giraldinho <47069662+giraldinho@users.noreply.github.com>
2019-02-05 14:47:42 -0800

    Update py_script.py

--- py_script.py
+++ py_script.py
@@ -2,6 +2,8 @@ import os
 import json
 import pandas
 
+## Using Spark
+
 from pyspark.sql import SparkSession
 spark = SparkSession \
 	.builder \
@@ -11,4 +13,17 @@ spark = SparkSession \
 
 path = '/Users/danielgiraldo/Documents/Classes/Spring_2019/ISE_599/Kiana_Analytics'
 
-df = spark.read.json(path)
+# create dataframe
+d0 = spark.read.json(path)
+
+# data cleaning
+
+d0.drop('_corrupt_record')
+
+
+d0.printSchema()
+
+# different stores
+d0.select('Store').distinct().show() 
+d0.crosstab('Store','Level').show()
+
